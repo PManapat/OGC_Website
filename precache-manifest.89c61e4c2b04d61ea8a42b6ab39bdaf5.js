da63aeaad4a498dd2d14c7b46c54a4ea167f1e82
pmanapat <philmanapat@gmail.com>
2020-09-26 13:27:40 -0400

    Updates

---
+++ precache-manifest.89c61e4c2b04d61ea8a42b6ab39bdaf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d01437d6327d39ed4046609be540fa",
+    "url": "/OGC_Website/index.html"
+  },
+  {
+    "revision": "3d14117c801e5bd8b00f",
+    "url": "/OGC_Website/static/css/main.e740a76d.chunk.css"
+  },
+  {
+    "revision": "9128d26167b6557c77a3",
+    "url": "/OGC_Website/static/js/2.845459dc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/OGC_Website/static/js/2.845459dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d14117c801e5bd8b00f",
+    "url": "/OGC_Website/static/js/main.36786482.chunk.js"
+  },
+  {
+    "revision": "4291d4f40a06b75203b9",
+    "url": "/OGC_Website/static/js/runtime-main.790c7372.js"
+  }
+]);
