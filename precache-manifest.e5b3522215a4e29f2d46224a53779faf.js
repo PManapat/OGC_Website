5856a116660f7cdab3500b824a9186e7c428a48b
pmanapat <philmanapat@gmail.com>
2020-10-29 17:26:17 -0400

    Updates

--- precache-manifest.e5b3522215a4e29f2d46224a53779faf.js
+++ precache-manifest.e5b3522215a4e29f2d46224a53779faf.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7aa680940a7870464118f59f19af5e16",
+    "revision": "5296a3d2b8271362f864f30382443d79",
     "url": "/OGC_Website/index.html"
   },
   {
-    "revision": "8ba7874d866597efd86c",
-    "url": "/OGC_Website/static/css/main.2588ec4d.chunk.css"
+    "revision": "60dce28db3401feb409c",
+    "url": "/OGC_Website/static/css/main.5ecb34ef.chunk.css"
   },
   {
     "revision": "4a823079bdb845880a13",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/OGC_Website/static/js/2.65c48552.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8ba7874d866597efd86c",
-    "url": "/OGC_Website/static/js/main.bfe2db33.chunk.js"
+    "revision": "60dce28db3401feb409c",
+    "url": "/OGC_Website/static/js/main.65dce8f4.chunk.js"
   },
   {
     "revision": "4291d4f40a06b75203b9",
