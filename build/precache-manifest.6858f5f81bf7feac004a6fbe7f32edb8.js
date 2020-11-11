c7f35f2aaaf4fee8e0cedb737fc611dca0aec819
pmanapat <philmanapat@gmail.com>
2020-11-10 22:06:08 -0500

    updated font size for page

--- build/precache-manifest.6858f5f81bf7feac004a6fbe7f32edb8.js
+++ build/precache-manifest.6858f5f81bf7feac004a6fbe7f32edb8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "630a106b1d207a7ae959df30e226aae0",
+    "revision": "71067940c17bc5b0618502cc92c94bd0",
     "url": "/OGC_Website/index.html"
   },
   {
-    "revision": "e0a3b249430d8742a0cd",
-    "url": "/OGC_Website/static/css/main.b6421e42.chunk.css"
+    "revision": "f9994384ecb11ae21b03",
+    "url": "/OGC_Website/static/css/main.94195291.chunk.css"
   },
   {
     "revision": "424c67722ac09d753410",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/OGC_Website/static/js/2.83114b36.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e0a3b249430d8742a0cd",
+    "revision": "f9994384ecb11ae21b03",
     "url": "/OGC_Website/static/js/main.9a86557d.chunk.js"
   },
   {
