70c651b923a2a21879e1fdf81abfacfeab2fa10f
pmanapat <philmanapat@gmail.com>
2020-11-17 00:23:18 -0500

    Updates

---
+++ precache-manifest.7087619685cfe34da8a16ba2c923ddf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4d68a5654cb96436f6791437b22e82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1e70256e13aae5f06de",
+    "url": "/static/css/main.12335587.chunk.css"
+  },
+  {
+    "revision": "424c67722ac09d753410",
+    "url": "/static/js/2.83114b36.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.83114b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e70256e13aae5f06de",
+    "url": "/static/js/main.1f72dd10.chunk.js"
+  },
+  {
+    "revision": "c521b6d354e5129adbca",
+    "url": "/static/js/runtime-main.0c5d968b.js"
+  }
+]);
